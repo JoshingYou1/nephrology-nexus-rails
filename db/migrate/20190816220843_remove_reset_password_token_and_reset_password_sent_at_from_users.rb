0979c23221e482c98810011171c0438a8bb00e39
Joshua Drumm <joshing_you1@yahoo.com>
2019-08-16 23:19:16 -0400

    First commit

---
+++ db/migrate/20190816220843_remove_reset_password_token_and_reset_password_sent_at_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveResetPasswordTokenAndResetPasswordSentAtFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+  end
+end
